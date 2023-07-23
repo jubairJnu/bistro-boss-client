8fab2268accd8af4e794c99522bff695210755cf
jubairJnu <jubair2810@gmail.com>
2023-07-24 00:21:54 +0600

    added footer and dynamic title

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,14 @@
 
 import Header from '../pages/shared/Header/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shared/footer/Footer';
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
       
     </div>
   );
---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+import { Helmet } from 'react-helmet-async';
+
+
+const Menu = () => {
+  return (
+    <div >
+      <Helmet>
+        <title>Bistro Boss | Menu</title>       
+      </Helmet>
+      <div className="mb-20">
+      <h1>this is menu</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
