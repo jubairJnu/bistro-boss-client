235e9a733768289b233b4e4cf73897d4556aee31
jubairJnu <jubair2810@gmail.com>
2023-07-29 21:45:46 +0600

    added payment

--- src/pages/Dashboard/CheckOut.jsx
+++ src/pages/Dashboard/CheckOut.jsx
@@ -1,64 +1,119 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect } from 'react';
 import { useState } from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import useAuth from '../../hooks/useAuth';
 
 
 
-const CheckOut = () => {
-const stripe = useStripe();
-const elements = useElements();
-const [cardError, setCardError] = useState('')
-const handleSubmit = async(event)=>{
-  event.preventDefault();
-  if(!stripe || !elements){
-    return;
-  }
-  
-  const card = elements.getElement(CardElement);
-   
-  if(card == null){
-    return;
-  }
+const CheckOut = ({cart, price }) => {
 
-  const {error, paymentMethod} = await stripe.createPaymentMethod({
-    type:'card',
-    card,
-  });
-  if(error){
-    setCardError(error.message)
-    console.log('card error', error)
+  const [axiosSecure] = useAxiosSecure();
+  const stripe = useStripe();
+  const elements = useElements();
+  const {user} = useAuth();
+  const [cardError, setCardError] = useState('')
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transjectionId, setTrasjectionId] = useState('');
+
+  useEffect(() => {
+    axiosSecure.post('/create-payment-intent',  {price} )
+      .then(res => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      })
+  }, [price, axiosSecure]);
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card,
+    });
+    if (error) {
+      setCardError(error.message)
+      console.log('card error', error)
+    }
+    else {
+      setCardError('')
+      console.log('paymentMethod', paymentMethod)
+    }
+setProcessing(true);
+    const {paymentIntent, error:confirmCardError} = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: user?.displayName || 'annonymous',
+            email: user?.email || 'annonymous'
+          },
+        },
+      },
+    );
+    if(confirmCardError){
+console.log(confirmCardError)
+    }
+setProcessing(false)
+    console.log(paymentIntent);
+    if(paymentIntent.status.succeeded){
+      setTrasjectionId(paymentIntent.id)
+      // const transjectionId =paymentIntent.id;
+      const payment ={
+        email: user?.email,
+        transjectionId: paymentIntent.id,
+        price,
+        quantity: cart.length,
+        items: cart.map(item=> item._id),
+        itemName: cart.map(item=> item.name)
+      }
+      axiosSecure.post('/payment', payment)
+      .then(res=>{
+        console.log(res.data);
+      })
+    }
+
   }
-  else{
-    setCardError('')
-    console.log('paymentMethod', paymentMethod)
-  }
-}
 
 
 
   return (
     <div>
-      <form className='m-10 w-2/3'  onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+      <form className='m-10 w-2/3' onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button className='btn btn-primary btn-sm mt-5' type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-    {cardError && <p className='text-red-600'>{cardError} </p>}
+          }}
+        />
+        <button className='btn btn-primary btn-sm mt-5' type="submit" disabled={!stripe || !clientSecret || processing}>
+          Pay
+        </button>
+      </form>
+      {cardError && <p className='text-red-600'>{cardError} </p>}
+      {transjectionId && <p className='text-green-500'>Transjection successfully with TrxId {transjectionId} </p>}
     </div>
   );
 };
--- src/pages/Dashboard/Payment.jsx
+++ src/pages/Dashboard/Payment.jsx
@@ -2,16 +2,20 @@ import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../component/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckOut from "./CheckOut";
+import useCart from "../../hooks/useCart";
 
-const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_PK);
+const stripePromise = loadStripe(`${import.meta.env.VITE_PAYMENT_PK}`);  //$dia template string a dite hobe as cs. 
 const Payment = () => {
+  const [cart] = useCart();
+  const total = cart.reduce((sum, item) => item.price + sum, 0);
+  const price = parseFloat(total.toFixed(2))
   return (
     <div className="w-full">
       <SectionTitle heading="payment" sunmHeading="Please Process"></SectionTitle>
     
 
       <Elements stripe={stripePromise}>
-        <CheckOut></CheckOut>
+        <CheckOut cart={cart} price ={price} ></CheckOut>
       </Elements>
 
     </div>
